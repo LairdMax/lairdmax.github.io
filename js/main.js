8d3777f608fbb7eefad2889ea043c29ce66239f5
LairdMax <63568555+LairdMax@users.noreply.github.com>
2022-11-08 12:59:42 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,8 +130,8 @@ $('#hitokoto').click(function () {
 //获取天气
 //请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
 //请前往 https://dev.qweather.com/ 申请 key
-const add_id = "wrknltonr0foslhs"; // app_id
-const app_secret = "Nlh1c0F6d0ZDU2pDR0J3YVBVbkhudz09"; // app_secret
+const add_id = "wemoq7t0uuixmovq"; // app_id
+const app_secret = "YVFBL0o5aFJiOU5pdCtnemV4UzBRZz09"; // app_secret
 const key = "fe379a3d60004a228454fc033acc1461" // key
 function getWeather() {
     fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
