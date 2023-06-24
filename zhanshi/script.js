76cb8e75882d4763925cf6aa0c93f503727a48d7
LairdMax <63568555+LairdMax@users.noreply.github.com>
2023-06-24 17:57:03 +0800

    Update script.js

--- zhanshi/script.js
+++ zhanshi/script.js
@@ -7,48 +7,39 @@ function loadImages() {
     .then((html) => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(html, 'text/html');
-      var links = doc.querySelectorAll('a');
+      var links = Array.from(doc.querySelectorAll('a'));
 
-      var imagePromises = [];
-
-      links.forEach((link) => {
-        var fileName = link.getAttribute('href');
-
-        if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg') || fileName.endsWith('.png')) {
-          var imagePromise = new Promise((resolve, reject) => {
-            var image = new Image();
-            image.onload = function () {
-              resolve({ element: image, src: image.src });
-            };
-            image.onerror = function () {
-              reject(new Error('Failed to load image: ' + image.src));
-            };
-            image.src = imageDirectory + fileName;
-          });
-
-          imagePromises.push(imagePromise);
-        }
-      });
+      var imagePromises = links
+        .filter((link) => isImageFile(link.getAttribute('href')))
+        .map((link) => {
+          var fileName = link.getAttribute('href');
+          var imageUrl = imageDirectory + fileName;
+          return loadImage(imageUrl)
+            .then((image) => {
+              var exifData = getExifData(image);
+              return {
+                element: image,
+                src: image.src,
+                exifData: exifData
+              };
+            })
+            .catch((error) => {
+              console.log('Failed to load image:', imageUrl);
+              console.log(error);
+              return null;
+            });
+        });
 
       Promise.all(imagePromises)
         .then((images) => {
-          images.sort((a, b) => {
-            var aTime = getExifDateTime(a.element);
-            var bTime = getExifDateTime(b.element);
-            return bTime - aTime;
-          });
+          images = images.filter((image) => image !== null);
+          images.sort((a, b) => compareExifDateTime(a.exifData, b.exifData));
 
           images.forEach((image) => {
-            var container = document.createElement('div');
-            container.classList.add('image-container');
+            var container = createImageContainer(image.src);
+            var exifInfo = createExifInfo(image.exifData);
 
-            var imgElement = document.createElement('img');
-            imgElement.src = image.src;
-
-            var exifInfo = document.createElement('p');
-            exifInfo.textContent = getExifDataString(image.element);
-
-            container.appendChild(imgElement);
+            container.appendChild(image.element);
             container.appendChild(exifInfo);
             imageContainer.appendChild(container);
           });
@@ -62,15 +53,55 @@ function loadImages() {
     });
 }
 
-function getExifDateTime(image) {
-  var exifData = EXIF.getAllTags(image);
+function isImageFile(fileName) {
+  return /\.(jpg|jpeg|png)$/i.test(fileName);
+}
+
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    var image = new Image();
+    image.onload = function () {
+      resolve(image);
+    };
+    image.onerror = function () {
+      reject(new Error('Failed to load image: ' + url));
+    };
+    image.src = url;
+  });
+}
+
+function getExifData(image) {
+  return EXIF.getAllTags(image);
+}
+
+function compareExifDateTime(a, b) {
+  var aTime = getExifDateTime(a);
+  var bTime = getExifDateTime(b);
+  return bTime - aTime;
+}
+
+function getExifDateTime(exifData) {
   var dateTime = exifData.DateTimeOriginal || exifData.DateTimeDigitized || exifData.DateTime;
   return dateTime ? Date.parse(dateTime) : 0;
 }
 
-function getExifDataString(image) {
-  var exifData = EXIF.pretty(image);
-  return exifData;
+function createImageContainer(src) {
+  var container = document.createElement('div');
+  container.classList.add('image-container');
+
+  var imgElement = document.createElement('img');
+  imgElement.src = src;
+
+  container.appendChild(imgElement);
+
+  return container;
+}
+
+function createExifInfo(exifData) {
+  var exifInfo = document.createElement('p');
+  exifInfo.textContent = JSON.stringify(exifData, null, 2);
+
+  return exifInfo;
 }
 
 window.onload = loadImages;
