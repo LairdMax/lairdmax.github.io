c66bbb16a212b604a3aa295401aaa1fa4ee18661
LairdMax <63568555+LairdMax@users.noreply.github.com>
2023-06-24 17:37:46 +0800

    Update script.js

--- zhanshi/script.js
+++ zhanshi/script.js
@@ -1,5 +1,5 @@
 var imageContainer = document.getElementById('image-gallery');
-var imageDirectory = 'images/'; // 图片文件夹路径
+var imageDirectory = 'images/';
 
 function loadImages() {
   fetch(imageDirectory)
@@ -11,11 +11,9 @@ function loadImages() {
 
       var imagePromises = [];
 
-      // 遍历文件夹中的所有图片
       links.forEach((link) => {
         var fileName = link.getAttribute('href');
 
-        // 筛选出图片文件
         if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg') || fileName.endsWith('.png')) {
           var imagePromise = new Promise((resolve, reject) => {
             var image = new Image();
@@ -32,18 +30,14 @@ function loadImages() {
         }
       });
 
-      // 等待所有图片加载完成
       Promise.all(imagePromises)
         .then((images) => {
-          // 根据拍摄时间进行排序
           images.sort((a, b) => {
             var aTime = getExifDateTime(a.element);
             var bTime = getExifDateTime(b.element);
-
             return bTime - aTime;
           });
 
-          // 显示排序后的照片和EXIF信息
           images.forEach((image) => {
             var container = document.createElement('div');
             container.classList.add('image-container');
