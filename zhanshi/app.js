faa81915e512fb301443ba7c87d66b23bc7624fc
LairdMax <63568555+LairdMax@users.noreply.github.com>
2023-06-28 08:38:57 +0800

    Create app.js

---
+++ zhanshi/app.js
@@ -0,0 +1,42 @@
+new Vue({
+  el: '#app',
+  data: {
+    photos: []
+  },
+  mounted() {
+    this.fetchPhotos();
+  },
+  methods: {
+    fetchPhotos() {
+      const apiUrl = 'https://api.github.com/repos/username/repo/contents/photos';
+      // 使用实际的GitHub用户名和存储照片的仓库替换上述的"username/repo"
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          this.photos = data.map(photo => ({
+            id: photo.name,
+            url: photo.download_url,
+            name: photo.name,
+            showExif: false,
+            exif: {}
+          }));
+        });
+    },
+    showExif(photo) {
+      photo.showExif = !photo.showExif;
+      if (photo.showExif && Object.keys(photo.exif).length === 0) {
+        this.fetchExif(photo);
+      }
+    },
+    fetchExif(photo) {
+      // 使用exif.js获取照片的EXIF信息，并将其存储到photo.exif对象中
+      // 在这里添加获取EXIF信息的代码
+      // 示例：
+      photo.exif = {
+        shootTime: '2021-09-15 12:30',
+        device: 'Canon EOS 5D Mark IV',
+        parameters: 'Aperture: f/2.8, Shutter Speed: 1/1000s, ISO: 200'
+      };
+    }
+  }
+});
